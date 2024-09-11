f23869b963972111edb7834fde81844eca42033d
diegoregino <diegoregino1994@gmail.com>
2024-09-11 01:48:01 -0500

    fix: draggable error

--- package/src/containers/PopupsProvider.tsx
+++ package/src/containers/PopupsProvider.tsx
@@ -3,13 +3,13 @@ import Button from '../components/Button/Button';
 import { popupContainerStyles, popupStyles } from '../constants/styles';
 import { usePopup } from '../hooks/usePopup';
 const PopupsProvider = () => {
-
-  const {addPopup, popups, deletePopup} = usePopup()
+  const { addPopup, popups, deletePopup } = usePopup();
+  const DraggableComponent: any = Draggable;
 
   return (
     <div style={popupContainerStyles} id="container">
       {popups.map((popup) => (
-        <Draggable key={popup.id} bounds="parent">
+        <DraggableComponent key={popup.id} bounds="parent">
           <div style={popupStyles}>
             <h4>Title: Popup{popup.type}</h4>
             <p>Component {popup.type}</p>
@@ -22,7 +22,7 @@ const PopupsProvider = () => {
               Close
             </Button>
           </div>
-        </Draggable>
+        </DraggableComponent>
       ))}
       <Button
         variant="primary"
